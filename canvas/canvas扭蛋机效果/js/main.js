52f723d6514344f9bbe820381c74636288894f72
chrischen <chrischen0405@gmail.com>
2018-09-29 11:21:17 +0800

    canvas扭蛋机功能完成

--- canvas/canvas扭蛋机效果/js/main.js
+++ canvas/canvas扭蛋机效果/js/main.js
@@ -8,13 +8,14 @@ var ballList = [ball1, ball2, ball3, ball4];
 var ballNum = 4;
 var awardList = [];
 var timer;
+var award = document.getElementById('awardBall');
+var message = document.getElementById('message');
 
-function init() {
-    for (let i = 0; i < ballNum; i++) {
+function init() {//初始化
+    for (let i = 0; i < ballNum; i++) {//随机生成各色小球
         let index = Math.floor(4 * Math.random());
         awardList[i] = new Ball(index, ballList[index]);
     }
-    console.log(awardList[3]);
     window.clearInterval(timer);
     timer = setInterval(function () {
         ctx.clearRect(0, 0, canvas.width, canvas.height);//清空画布
@@ -25,11 +26,11 @@ function init() {
 }
 
 function Ball(index, img) {
-    this.r = 30;
-    this.x = this.rand(canvas.width - this.r * 2);
-    this.y = this.rand(canvas.height - this.r * 2);
-    this.color = index;
-    this.img = img;
+    this.r = 30;//小球半径
+    this.x = this.rand(canvas.width - this.r * 2);//小球初始横坐标
+    this.y = this.rand(canvas.height - this.r * 2);//小球初始纵坐标
+    this.color = index;//小球颜色，以下标表示
+    this.img = img;//小球素材
     do {
         this.speedX = this.rand(20) - 10;
     } while (this.speedX < 5);//小球横坐标改变速度
@@ -57,14 +58,15 @@ Ball.prototype = {
         if (this.y < 0) {//小球碰到上边界，纵坐标速度变为正
             this.speedY = Math.abs(this.speedY);
         }
-        ctx.drawImage(this.img, this.x, this.y, 60, 60);
+        ctx.drawImage(this.img, this.x, this.y, 60, 60);//绘制小球
     }
 }
 
 function play() {
-    if (awardList.length === 0) {
+    if (awardList.length === 0) {//奖池中没有小球
         alert('重新开始！');
         init();
+        message.innerText = '点击抽奖';
     } else {
         window.clearInterval(timer);
         let r = awardList.pop();
@@ -74,20 +76,37 @@ function play() {
                 awardList[i].run();
             }//使小球运动
         }, 15);
-        switch (r.color) {
+        switch (r.color) {//小球掉落动画
             case 0:
-                alert('紫球！');
+                award.setAttribute('class', 'dropBall1');
                 break;
             case 1:
-                alert('绿球！');
+                award.setAttribute('class', 'dropBall2');
                 break;
             case 2:
-                alert('黄球！');
+                award.setAttribute('class', 'dropBall3');
                 break;
             case 3:
-                alert('红球！');
+                award.setAttribute('class', 'dropBall4');
                 break;
         }
+        setTimeout(function () {//扭蛋成功提示
+            award.setAttribute('class', '');
+            switch (r.color) {
+                case 0:
+                    message.innerText = '紫球！';
+                    break;
+                case 1:
+                    message.innerText = '绿球！';
+                    break;
+                case 2:
+                    message.innerText = '黄球！';
+                    break;
+                case 3:
+                    message.innerText = '红球！';
+                    break;
+            }
+        }, 1100);
     }
 }
 
