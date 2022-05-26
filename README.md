1b10fa971b9e2ee1e8555ff833a2af302609a15a
chrischen0405 <chrischen0405@gmail.com>
2022-05-26 18:24:54 +0800

    feat: ecahrts雷达图tooltip显示单轴数据

--- README.md
+++ README.md
@@ -29,6 +29,8 @@
         - [2 双飞翼布局](#2-双飞翼布局)
         - [3 按钮涟漪](#3-按钮涟漪)
         - [4 绘制三角形](#4-绘制三角形)
+    - [七、Echarts](七Echarts)
+        - [1 ecahrts雷达图tooltip显示单轴数据](#1-ecahrts雷达图tooltip显示单轴数据)
 
 <!-- /TOC -->
 
@@ -161,3 +163,9 @@ CSS经典三列布局，左右两栏固定，中间自适应
 方向依次为上、下、左、右的三角形
 
 <a href="https://chrischen0405.github.io/Demo/HTML&CSS/6-4triangle/index.html" target="_blank">预览</a>
+
+## 七、Echarts
+
+Echarts的一些配置项
+
+### 1 ecahrts雷达图tooltip显示单轴数据
---
+++ echarts/7-1radar.js
@@ -0,0 +1,88 @@
+// 示例数据
+const dataList = [
+    {
+        name: '小明',
+        data: [54, 32, 56, 30, 95, 35],
+        color: '#5470c6',
+        unit: '分'
+    },
+    {
+        name: '小红',
+        data: [84, 63, 23, 59, 78, 95],
+        color: '#91cc75',
+        unit: '分'
+    },
+    {
+        name: '小刚',
+        data: [14, 98, 63, 74, 40, 67],
+        color: '#fac858',
+        unit: '分'
+    }
+]
+
+// 雷达图的指示器，用来指定雷达图中的多个变量（维度）
+const indicator = [
+    { name: '语文', max: 100 },
+    { name: '数学', max: 100 },
+    { name: '英语', max: 100 },
+    { name: '物理', max: 100 },
+    { name: '化学', max: 100 },
+    { name: '生物', max: 100 }
+]
+
+// 根据传入数据下标生成series
+const buildSeries = function (dataIndex) {
+    const data = dataList[dataIndex].data
+    const helper = data.map((item, index) => {
+        const arr = new Array(data.length)
+        arr.splice(index, 1, item)
+        return arr
+    })
+    return [data, ...helper].map((item, index) => {
+        return {
+            name: dataList[dataIndex].name,
+            type: 'radar',
+            symbol: index === 0 ? 'circle' : 'none',
+            symbolSize: 4,
+            itemStyle: {
+                color: '#fff'
+            },
+            lineStyle: {
+                color: index === 0 ? dataList[dataIndex].color : 'transparent'
+            },
+            areaStyle: {
+                color: index === 0 ? dataList[dataIndex].color : 'transparent',
+                opacity: 0.5
+            },
+            tooltip: {
+                show: index === 0 ? false : true,
+                formatter: () => {
+                    let res = indicator[index - 1].name + '：<br>'
+                    for (let x of dataList) {
+                        let str = '<i style="display: inline-block;width: 10px;height: 10px;background: ' +
+                            x.color + ';margin-right: 5px;border-radius: 50%;}"></i>' +
+                            x.name + '：' + x.data[index - 1] + x.unit + '<br>'
+                        res += str
+                    }
+                    return res
+
+                }
+            },
+            z: index === 0 ? 1 : 2,
+            data: [item]
+        }
+    })
+}
+
+const series = []
+for (let i in dataList) {
+    series.push(...buildSeries(i))
+}
+
+option = {
+    tooltip: {},
+    radar: {
+        indicator: indicator
+    },
+    series: series
+}
